bfe7f5db004f86f5a3d14b39ceaa68473ec7e799
Ahmed Hussnain <hussnain.ahmed@tieto.com>
2018-03-18 19:32:55 +0200

    adding r-shiny-app

--- r-shiny-app/ui.R
+++ r-shiny-app/ui.R
@@ -1,14 +1,12 @@
 library(shiny)
 library(ggplot2)
 
-pageWithSidebar(
+fluidPage(
   headerPanel('Stackoverflow Survey Data'),
   sidebarPanel(
     selectInput('country', 'Select Country', choices = df$Country, selected = "Pakistan"),
     selectInput('question', 'Select Question',choices = c('HaveWorkedLanguage','WantWorkLanguage','HaveWorkedFramework','WantWorkFramework','HaveWorkedPlatform','WantWorkPlatform'),selected='HaveWorkedLanguage'),
-    width = 3
-    #numericInput('clusters', 'Cluster count', 3,
-     #            min = 1, max = 9)
+    width = 4
   ),
   mainPanel(
     plotOutput('plot1',width = "100%", height = "300px")
